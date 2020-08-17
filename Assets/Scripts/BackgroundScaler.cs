4ba8d9cb2010e4a30b9953f247313a71eaa216a3
Shajid Hossain <sajidhossainhemal@gmail.com>
2020-08-17 14:05:37 +0600

    Background Setup

---
+++ Assets/Scripts/BackgroundScaler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float height = Camera.main.orthographicSize * 2f;
+        float width = height * Screen.width / Screen.height;
+
+        if(gameObject.name == "Background") {
+            transform.localScale = new Vector3(width, height, transform.localScale.z);
+        } else {
+            transform.localScale = new Vector3(width + 3f, 3f, transform.localScale.z);
+        }
+    }
+}
